1101a3c298bf7c46b81a2382f369605776fa3416
CHSI\si1206 <samchen@ch-si.com.tw>
2021-04-15 17:47:58 +0800

    排版

--- src/main/java/com/example/socket/controller/SocketController.java
+++ src/main/java/com/example/socket/controller/SocketController.java
@@ -18,42 +18,45 @@ import java.util.Map;
 public class SocketController extends HttpServlet {
     @Autowired
     SocketService socketService;
+
     @GetMapping("/")
     public String index(HttpSession session) {
-        session.setAttribute("status",null);
+        session.setAttribute("status", null);
         return "index";
     }
+
     @PostMapping("/connect")
-    public String connect(@RequestParam(value = "address",required = false)String address,
-                          @RequestParam(value = "port",required = false)int port,
-                          Model model,HttpSession session) throws UnknownHostException {
-        socketService.connect(address,port,session);
+    public String connect(@RequestParam(value = "address", required = false) String address,
+                          @RequestParam(value = "port", required = false) int port,
+                          Model model, HttpSession session) throws UnknownHostException {
+        socketService.connect(address, port, session);
         return "index";
     }
 
     @PostMapping("/close")
-    public String close(Model model,HttpSession session) throws IOException {
+    public String close(Model model, HttpSession session) throws IOException {
         socketService.close(session);
         return "index";
     }
 
     @PostMapping("/send")
-    public String send(@RequestParam(value = "io",required = false)String io ,Model model,HttpSession session) throws IOException {
-        if(session.getAttribute("status")!=null) {
-                model.addAttribute("display",socketService.send(io));
-            } else {
-                model.addAttribute("fault", "  請先連線");
-            }
+    public String send(@RequestParam(value = "io", required = false) String io, Model model, HttpSession session) throws IOException {
+        if (session.getAttribute("status") != null) {
+            model.addAttribute("display", socketService.send(io));
+        } else {
+            model.addAttribute("fault", "  請先連線");
+        }
         return "index";
     }
+
     @PostMapping("/transaction")
-    public String transaction(@RequestParam(value = "transaction",required = false)String transaction ,Model model,HttpSession session) throws IOException {
-        if(session.getAttribute("status")!=null) {
+    public String transaction(@RequestParam(value = "transaction", required = false) String transaction, Model model, HttpSession session) throws IOException {
+        if (session.getAttribute("status") != null) {
             session.setAttribute("transaction", transaction);
             socketService.transaction(transaction);
             System.out.println(transaction);
-        }else{
-            model.addAttribute("fault","  請先連線");
+        } else {
+            model.addAttribute("fault", "  請先連線");
         }
         return "index";
     }
--- src/main/java/com/example/socket/service/SocketService.java
+++ src/main/java/com/example/socket/service/SocketService.java
@@ -14,13 +14,15 @@ import java.io.PrintWriter;
 import java.net.InetAddress;
 import java.net.Socket;
 import java.net.UnknownHostException;
+
 @Service
 public class SocketService extends HttpServlet {
     private static boolean open = false;
-        Socket socket;
-    public void connect(String address,int port,HttpSession session) throws UnknownHostException {
+    Socket socket;
+
+    public void connect(String address, int port, HttpSession session) throws UnknownHostException {
         String data;
-        BufferedReader key_in = new BufferedReader(new InputStreamReader(	System.in));
+        BufferedReader key_in = new BufferedReader(new InputStreamReader(System.in));
         BufferedReader s_in;
         PrintWriter s_out;
         InetAddress iNetAddress = InetAddress.getLocalHost();
@@ -28,28 +30,30 @@ public class SocketService extends HttpServlet {
         try {
             // 呼叫此建構子的同時,即根據參數的IP與PORT去連接ServerSocket
             socket = new Socket(address, port);
-            open=true;
+            open = true;
             System.out.println(socket);
             s_in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
             s_out = new PrintWriter(socket.getOutputStream());
-            if(open) {
-                session.setAttribute("status","連線中");
+            if (open) {
+                session.setAttribute("status", "連線中");
                 System.out.println("連線成功");
-            }else{
-                session.setAttribute("status","無法連線");
+            } else {
+                session.setAttribute("status", "無法連線");
             }
         } catch (IOException ioe) {
             System.out.println("無法連接主機...");
         }
     }
+
     public void close(HttpSession session) throws IOException {
-        if(session.getAttribute("status")!=null) {
+        if (session.getAttribute("status") != null) {
             socket.close();
-            session.setAttribute("transaction",null);
+            session.setAttribute("transaction", null);
             session.setAttribute("status", null);
             System.out.println("關閉連線");
         }
     }
+
     public String send(String a) throws IOException {
         BufferedReader s_in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
         PrintWriter s_out = new PrintWriter(socket.getOutputStream());
@@ -57,6 +61,7 @@ public class SocketService extends HttpServlet {
         s_out.flush();
         return s_in.readLine();
     }
+
     public void transaction(String type) throws IOException {
         PrintWriter s_out = new PrintWriter(socket.getOutputStream());
         s_out.println(type);
