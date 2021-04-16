cc7cbf149229dfbf4ccb4e052522bf9313822749
qsxazqoo <73749423+qsxazqoo@users.noreply.github.com>
2021-04-16 09:40:30 +0800

    主機重複連線判斷

--- src/main/java/com/example/socket/service/SocketService.java
+++ src/main/java/com/example/socket/service/SocketService.java
@@ -19,21 +19,18 @@ import java.net.UnknownHostException;
 public class SocketService extends HttpServlet {
     private static boolean open = false;
     Socket socket;
+    BufferedReader s_in;
+    PrintWriter s_out;
 
     public void connect(String address, int port, HttpSession session) throws UnknownHostException {
         String data;
         BufferedReader key_in = new BufferedReader(new InputStreamReader(System.in));
-        BufferedReader s_in;
-        PrintWriter s_out;
         InetAddress iNetAddress = InetAddress.getLocalHost();
         String hostname = iNetAddress.getHostName();
         try {
             // 呼叫此建構子的同時,即根據參數的IP與PORT去連接ServerSocket
             socket = new Socket(address, port);
             open = true;
-            System.out.println(socket);
-            s_in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-            s_out = new PrintWriter(socket.getOutputStream());
             if (open) {
                 session.setAttribute("status", "連線中");
                 System.out.println("連線成功");
@@ -55,15 +52,15 @@ public class SocketService extends HttpServlet {
     }
 
     public String send(String a) throws IOException {
-        BufferedReader s_in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-        PrintWriter s_out = new PrintWriter(socket.getOutputStream());
+        s_in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        s_out = new PrintWriter(socket.getOutputStream());
         s_out.println(a);
         s_out.flush();
         return s_in.readLine();
     }
 
     public void transaction(String type) throws IOException {
-        PrintWriter s_out = new PrintWriter(socket.getOutputStream());
+        s_out = new PrintWriter(socket.getOutputStream());
         s_out.println(type);
         s_out.flush();
     }
