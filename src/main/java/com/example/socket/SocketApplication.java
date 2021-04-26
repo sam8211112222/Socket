57b55fe652764bf22b52da3ab06afb40a9cd230d
qsxazqoo <73749423+qsxazqoo@users.noreply.github.com>
2021-04-26 16:43:42 +0800

    log

--- src/main/java/com/example/socket/SocketApplication.java
+++ src/main/java/com/example/socket/SocketApplication.java
@@ -1,11 +1,11 @@
 package com.example.socket;
 
+import com.example.socket.service.SocketService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SocketApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(SocketApplication.class, args);
 	}
--- src/main/java/com/example/socket/service/SocketService.java
+++ src/main/java/com/example/socket/service/SocketService.java
@@ -1,9 +1,8 @@
 package com.example.socket.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.SessionAttribute;
-import org.springframework.web.bind.annotation.SessionAttributes;
 
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpSession;
@@ -21,7 +20,7 @@ public class SocketService extends HttpServlet {
     Socket socket;
     BufferedReader s_in;
     PrintWriter s_out;
-
+    private static final Logger logger = LoggerFactory.getLogger(SocketService.class);
     //與server連線
     public void connect(String address, int port, HttpSession session) throws UnknownHostException {
         String data;
@@ -34,9 +33,11 @@ public class SocketService extends HttpServlet {
             open = true;
             if (open) {
                 session.setAttribute("status", "連線中");
+                logger.info("連線成功");
                 System.out.println("連線成功");
             } else {
                 session.setAttribute("status", "無法連線");
+                logger.info("無法連線");
             }
         } catch (IOException ioe) {
             System.out.println("無法連接主機...");
@@ -49,6 +50,7 @@ public class SocketService extends HttpServlet {
             session.setAttribute("transaction", null);
             session.setAttribute("status", null);
             System.out.println("關閉連線");
+            logger.info("關閉連線");
         }
     }
     //送出String
@@ -56,13 +58,16 @@ public class SocketService extends HttpServlet {
         s_in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
         s_out = new PrintWriter(socket.getOutputStream());
         s_out.println(a);
+        logger.info("傳送字串: "+a);
         s_out.flush();
+        logger.info("接收資料: "+s_in.readLine());
         return s_in.readLine();
     }
     //送出交易代碼
     public void transaction(String type) throws IOException {
         s_out = new PrintWriter(socket.getOutputStream());
         s_out.println(type);
+        logger.info("交易代碼: "+type);
         s_out.flush();
     }
 }
